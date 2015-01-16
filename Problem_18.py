e43aeb68aaa755098ebc8f541b235828acb1e210
Daniel King <kingds3272@gmail.com>
2015-01-16 14:36:27 -0600

    Added solution to problem 19.

---
+++ Problem_18.py
@@ -0,0 +1,38 @@
+# Find the maximum total of moving on a path down the triangle through adjacent numbers
+
+# Create the triangle list from the raw values
+triangle_raw = open("Problem_18_Triangle.txt").read()
+triangle_rows = triangle_raw.split("\n")
+triangle = []
+for row in triangle_rows:
+	triangle.append(row.split(" "))
+
+# Brute force solution
+def brute_force():
+	max_total = 0
+	for row in triangle:
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+brute_force()
+
+
+a = 0
+for i in range(15):
+	
+
+
+print a
+
+
+

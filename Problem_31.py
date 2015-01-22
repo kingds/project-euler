0a5700315b9348991e659c3662874e034ba8f28a
Daniel King <kingds3272@gmail.com>
2015-01-22 16:15:47 -0600

    Got the solution working for problem 31. First 50 complete!

--- Problem_31.py
+++ Problem_31.py
@@ -2,65 +2,13 @@
 
 # Coins are 1p, 2p, 5p, 10p, 20p, 50p, 100p, 200p
 
-# coins = [200, 100, 50, 20, 10, 5, 2, 1]
-# combo_count = 0
-
-# import itertools
-
-# combos = []
-# for p in itertools.product(coins, repeat=3):
-# 	if sum(p) == 200:
-# 		combos.append(p)
-# 		combo_count += 1
-# print combos
-# print len(set(combos))
-# print combo_count
-
-# count = 0
-# for p100 in range(3):
-# 	total = p100 * 100
-# 	if total >= 200:
-# 		count += 1
-# 		break
-# 	for p50 in range(5):
-# 		total += p50 * 50
-# 		if total >= 200:
-# 			count += 1
-# 			break
-# 		for p20 in range(11):
-# 			total += p20 * 20
-# 			if total >= 200:
-# 				count += 1
-# 				break
-# 			for p10 in range(21):
-# 				total += p10 * 10
-# 				if total >= 200:
-# 					count += 1
-# 					break
-# 				for p5 in range(41):
-# 					total += p5 * 5
-# 					if total >= 200:
-# 						count += 1
-# 						break
-# 					for p2 in range(101):
-# 						total += p2 * 2
-# 						if total >= 200:
-# 							count += 1
-# 							break
-
-# print count
-
 count = 0
-for p200 in range(2):
-	total = 200 - 
-	for p100 in range(3):
-		for p50 in range(5):
-			for p20 in range(11):
-				for p10 in range(21):
-					for p5 in range(41):
-						for p2 in range(101):
+for a in range(0, 201, 200):
+	for b in range(a, 201, 100):
+		for c in range(b, 201, 50):
+			for d in range(c, 201, 20):
+				for e in range(d, 201, 10):
+					for f in range(e, 201, 5):
+						for g in range(f, 201, 2):
 							count += 1
 print count
-
-
-

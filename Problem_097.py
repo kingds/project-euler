7fc7d775e2b69d92c9db139abc1a36ce16edacee
Daniel King <kingds3272@gmail.com>
2015-01-26 11:51:09 -0600

    Sped up problem 97 using modulo. No need to process the whole number.

--- Problem_097.py
+++ Problem_097.py
@@ -1,14 +1,8 @@
 # There is a massive non-Mersenne prime with 2,357,207 digits: 28433x2^7830457 + 1
 # Find the last ten digits of this prime number.
 
-# print str(28433 * 2**7830457 + 1)[-10:]
+n = 2 ** 7830457
+n *= 28433
+n += 1
 
-x = 1
-total = 0
-for d in reversed(str(2**7830457)[-10 :]):
-	total += 28433 * int(d) * x
-	x *= 10
-
-total += 1
-
-print str(total)[-10 :]
+print n % 10000000000

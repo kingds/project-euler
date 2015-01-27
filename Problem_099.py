489f8d2f17c990b741c664db4bc413e265d580cd
Daniel King <kingds3272@gmail.com>
2015-01-26 22:23:41 -0600

    Got a fast, working solution for problem 99.

--- Problem_099.py
+++ Problem_099.py
@@ -1,5 +1,7 @@
 # Problem_099_Base_Exp.txt contains 1000 base exponent pairs. Determine which line number has the greatest numerical value
 
+from math import log
+
 pairs_raw = open("Problem_099_Base_Exp.txt").read().split("\n")
 pairs_dict = {}
 pairs = []
@@ -9,8 +11,17 @@ for pair in pairs_raw:
 	pairs.append((int(split_pair[0]), int(split_pair[1])))
 
 
+def greater(a, b):
+	if a[1] * log(a[0]) > b[1] * log(b[0]):
+		return 0
+	else:
+		return 1
 
+max_pair = pairs[0]
+for i in range(1, len(pairs)):
+	if greater(max_pair, pairs[i]) == 1:
+		max_pair = pairs[i]
 
-
+print pairs.index(max_pair) + 1
 	
 

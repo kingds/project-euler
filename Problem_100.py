093045711d6c5ce6918c8e41059b1a4680c44c18
Daniel King <kingds3272@gmail.com>
2015-01-28 15:42:01 -0600

    Working on a solution to problem 100.

--- Problem_100.py
+++ Problem_100.py
@@ -2,11 +2,29 @@
 
 # (b / b+r)*(b-1 / b+r-1) = 0.5,  b+r > 10^12
 
-solutions = []
-for b in range(1, 100):
-	for r in range(1, 100):
-		if (float(b) / (b+r)) * ((float(b)-1) / (b+r-1)) == 0.5:
-			solutions.append((b, r))
+# solutions = []
+# for b in range(1, 200):
+# 	for r in range(1, 200):
+# 		if b == 85 and r == 35:
+# 			print (float(b) / float(b+r)) * ((float(b)-1) / float(b+r-1)) == 0.5
 
-print solutions
+# 		if (float(b) / (b+r)) * ((float(b)-1) / (b+r-1)) == 0.5:
+# 			solutions.append((b, r))
+
+# print solutions
+
+def calc_r(b):
+	r = r = 0.5 * ((-2*b) + (8*b**2 - 8*b + 1)**0.5 + 1)
+	return r
+
+# b = 707106781185
+b = 707106781185
+
+while True:
+	r = calc_r(b)
+	# print r
+	if r.is_integer():
+		print b, r
+		print b + r
+	b += 1
 

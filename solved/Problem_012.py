28add9eec748a1d8c5674a4f8d51fab2a3071960
Daniel King <kingds3272@gmail.com>
2015-06-15 11:25:13 -0500

    Sped up solution to problem 12.

--- solved/Problem_012.py
+++ solved/Problem_012.py
@@ -3,18 +3,21 @@
 def number_of_factors(n):    
     return len(set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
 
-
 def triangle(n):
     return (n * (n + 1)) // 2 
 
 
 def main():
-    n = 1
+    n = 0
     while True:
-        print triangle(n), number_of_factors(triangle(n))
-        if number_of_factors(triangle(n)) > 500:
-            break
         n += 1
+        t = triangle(n)
+        if t % 2 != 0:
+            continue
+        if number_of_factors(t) > 500:
+            break
+        if t > 76576500:
+            break        
 
     return triangle(n)
 

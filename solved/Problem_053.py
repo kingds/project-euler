40f0be2b575fbadff5093e3736eedc0d8ae11445
Daniel King <kingds3272@gmail.com>
2015-06-30 16:59:11 -0500

    Working on speeding up solutions.

--- solved/Problem_053.py
+++ solved/Problem_053.py
@@ -1,17 +1,24 @@
 # How many, not necessarily distinct, values of nCr, for 1 <= n <= 100, are greater than one-million?
 
 def factorial(n):
-	product = 1
-	for i in xrange(1, n+1):
-		product *= i
-	return product
+    product = 1
+    for i in xrange(1, n+1):
+        product *= i
+    return product
 
 def combination(n, r):
-	return factorial(n)/(factorial(r) * factorial(n-r))
+    return factorial(n)/(factorial(r) * factorial(n-r))
 
-count = 0
-for n in range(1, 101):
-	for r in range(1, n):
-		if combination(n, r) > 1000000:
-			count += 1
-print count
+def main():
+    count = 0
+    for n in range(1, 101):
+        for r in range(1, n):
+            if combination(n, r) > 1000000:
+                count += 1
+    
+    return count
+
+
+if __name__ == "__main__":
+
+    print main()

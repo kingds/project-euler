6885ed2ee5d0017563149a907729c94739408bc0
Daniel King <kingds3272@gmail.com>
2015-07-01 16:48:16 -0500

    Working on speeding up and fixing solutions.

--- solved/Problem_059.py
+++ solved/Problem_059.py
@@ -2,57 +2,62 @@
 # Encryption key consists of 3 lower case characters
 
 # Lower case ASCII characters are in the range of 97-122. 
-
-
-
 # Bitwise XOR operator is ^
 
-cipher = open("Problem_059_Cipher.txt").read().replace("\n", "").split(",")
+def main():
+    # Open the cipher
+    cipher = open("Problem_059_Cipher.txt").read().replace("\n", "").split(",")
 
-c1 = []
-p = 0
-while p < len(cipher):
-	c1.append(cipher[p])
-	p += 3
+    # It is known that key is three letters that repeat cyclically, so split the cipher into three lists
+    c1 = []
+    p = 0
+    while p < len(cipher):
+        c1.append(cipher[p])
+        p += 3
 
-c2 = []
-p = 1
-while p < len(cipher):
-	c2.append(cipher[p])
-	p += 3
+    c2 = []
+    p = 1
+    while p < len(cipher):
+        c2.append(cipher[p])
+        p += 3
 
-c3 = []
-p = 2
-while p < len(cipher):
-	c3.append(cipher[p])
-	p += 3
+    c3 = []
+    p = 2
+    while p < len(cipher):
+        c3.append(cipher[p])
+        p += 3
 
-c1_space = max(set(c1), key=c1.count)
-c2_space = max(set(c2), key=c2.count)
-c3_space = max(set(c3), key=c3.count)
+    # Determine the "space" character for each key set.
+    # This assumes that space is the most common character in the message
+    c1_space = max(set(c1), key=c1.count)
+    c2_space = max(set(c2), key=c2.count)
+    c3_space = max(set(c3), key=c3.count)
 
-ascii_character = 32
+    # Space has the ascii value 32
+    ascii_character = 32
 
-c1_key = int(c1_space) ^ ascii_character
-c2_key = int(c2_space) ^ ascii_character
-c3_key = int(c3_space) ^ ascii_character
+    c1_key = int(c1_space) ^ ascii_character
+    c2_key = int(c2_space) ^ ascii_character
+    c3_key = int(c3_space) ^ ascii_character
 
-decoded = ""
-keys = [c1_key, c2_key, c3_key]
-for i in range(len(cipher)):
-	decoded += chr(int(cipher[i]) ^ keys[i % 3])
+    decoded = ""
+    keys = [c1_key, c2_key, c3_key]
+    for i in range(len(cipher)):
+        decoded += chr(int(cipher[i]) ^ keys[i % 3])
 
-print decoded
+    print decoded
 
-total = 0
-for c in c1:
-	total += int(c) ^ c1_key
-for c in c2:
-	total += int(c) ^ c2_key
-for c in c3:
-	total += int(c) ^ c3_key
+    total = 0
+    for c in c1:
+        total += int(c) ^ c1_key
+    for c in c2:
+        total += int(c) ^ c2_key
+    for c in c3:
+        total += int(c) ^ c3_key
 
-print "ASCII Total: " + str(total)
+    return total
 
 
+if __name__ == "__main__":
 
+    print main()
--- solved/Problem_060.py
+++ solved/Problem_060.py
@@ -1,75 +1,76 @@
 # Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 
 def is_prime(n):
-	if n < 2:
-		return False
-	elif n == 2:
-		return True
-	if not n & 1:
-		return False
-	sqr = int(n**(0.5)) + 1
-	for divisor in range(3, sqr, 2):
-		if n%divisor == 0:
-			return False
-	return True
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    if not n & 1:
+        return False
+    sqr = int(n**(0.5)) + 1
+    for divisor in range(3, sqr, 2):
+        if n%divisor == 0:
+            return False
+    return True
 
 def primes_sieve(limit):
-	a = [True] * limit                          
-	a[0] = a[1] = False
+    a = [True] * limit                          
+    a[0] = a[1] = False
 
-	for (i, isprime) in enumerate(a):
-		if isprime:
-			yield i
-			for n in xrange(i*i, limit, i):     
-				a[n] = False
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in xrange(i*i, limit, i):     
+                a[n] = False
 
 def permutations(iterable, r=None):
-	# permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
-	# permutations(range(3)) --> 012 021 102 120 201 210
-	pool = tuple(iterable)
-	n = len(pool)
-	r = n if r is None else r
-	if r > n:
-		return
-	indices = range(n)
-	cycles = range(n, n-r, -1)
-	yield tuple(pool[i] for i in indices[:r])
-	while n:
-		for i in reversed(range(r)):
-			cycles[i] -= 1
-			if cycles[i] == 0:
-				indices[i:] = indices[i+1:] + indices[i:i+1]
-				cycles[i] = n - i
-			else:
-				j = cycles[i]
-				indices[i], indices[-j] = indices[-j], indices[i]
-				yield tuple(pool[i] for i in indices[:r])
-				break
-		else:
-			return
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = range(n)
+    cycles = range(n, n-r, -1)
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
 
 def concat_prime(a, b):
-	if not is_prime(int(str(a) + str(b))):
-		return False
-	if not is_prime(int(str(b) + str(a))):
-		return False
-	return True
+    if not is_prime(int(str(a) + str(b))):
+        return False
+    if not is_prime(int(str(b) + str(a))):
+        return False
+    return True
+
+def main():
+    for a in primes_sieve(10000):
+        concat = [a]
+        for b in primes_sieve(10000):
+            if len(concat) == 5:
+                return sum(concat)
+                quit()
+            match = True
+            for c in concat:
+                if not concat_prime(b, c):
+                    match = False
+                    break
+            if match:
+                concat.append(b)
 
 
-for a in primes_sieve(10000):
-	concat = [a]
-	for b in primes_sieve(10000):
-		if len(concat) == 5:
-			print concat
-			print sum(concat)
-			quit()
-		match = True
-		for c in concat:
-			if not concat_prime(b, c):
-				match = False
-				break
-		if match:
-			concat.append(b)
+if __name__ == "__main__":
 
-
-			
+    print main()

b9778c7aea8f6ee94ac06bc7d2b3334d1de433c6
Daniel King <kingds3272@gmail.com>
2015-07-07 16:58:55 -0500

    Working on speeding up solutions.

--- solved/Problem_071.py
+++ solved/Problem_071.py
@@ -1,20 +1,23 @@
 # By listing the set of reduced proper fractions for d <= 1,000,000 in ascending order of size, find the numeratior of the fraction immediately to the left of 3/7
 
-value = 3.0/7.0
+def main():
+    value = 3.0/7.0
 
-min_difference = 1
-n_target = 0
-d_target = 0
-for d in range(10**6 + 1, 2, -1):
+    min_difference = 1
+    n_target = 0
+    d_target = 0
+    for d in range(10**6 + 1, 2, -1):
 
-	n = int(value * d)
-	difference = value - float(n)/float(d)
-	if difference < min_difference and d != 7 and difference != 0:
-		min_difference = difference
-		n_target = n
-		d_target = d
+        n = int(value * d)
+        difference = value - float(n)/float(d)
+        if difference < min_difference and d != 7 and difference != 0:
+            min_difference = difference
+            n_target = n
+            d_target = d
 
-print n_target
+    return n_target
 
 
+if __name__ == "__main__":
 
+    print main()
--- solved/Problem_073.py
+++ solved/Problem_073.py
@@ -1,16 +1,23 @@
-# How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d <= 12000
+# How many fractions lie between 1/3 and 1/2 in the sorted 
+# set of reduced proper fractions for d <= 12000
 
-value_1 = 1.0/3.0
-value_2 = 1.0/2.0
+def main():
+	value_1 = 1.0/3.0
+	value_2 = 1.0/2.0
+	fractions = set()
+	for denominator in range(6000, 12001):
+		n_1 = int(denominator * value_1) + 1
+		n_2 = int(denominator * value_2)
+		if denominator % 2 != 0:
+			n_2 += 1
+		for numerator in range(n_1, n_2):
+			fractions.add(float(numerator) / float(denominator))
 
-fractions = []
-count = 0
-for d in range(2, 12001):
-	n_1 = int(d * value_1) + 1
-	n_2 = int(d * value_2)
-	for n in range(n_1, n_2 + 1):
-		f = float(n) / float(d)
-		if f != value_1 and f != value_2:
-			fractions.append(f)
+	return len(fractions)
 
-print len(set(fractions))
+
+
+
+if __name__ == "__main__":
+
+	print main()

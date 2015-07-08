b09e9af0506c03ef8ac312aba86815bbc69d555d
Daniel King <kingds3272@gmail.com>
2015-07-08 14:36:24 -0500

    Speed improvements.

--- solved/Problem_060.py
+++ solved/Problem_060.py
@@ -1,4 +1,5 @@
-# Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+# Find the lowest sum for a set of five primes for which any two 
+# primes concatenate to produce another prime.
 
 def is_prime(n):
     if n < 2:
@@ -32,37 +33,38 @@ def concat_prime(a, b):
     return True
 
 def main():
-    # for a in primes_sieve(10000):
-    #     concat = [a]
-    #     for b in primes_sieve(10000):
-    #         if len(concat) == 5:
-    #             print concat
-    #             return sum(concat)
-    #         match = True
-    #         for c in concat:
-    #             if not concat_prime(b, c):
-    #                 match = False
-    #                 break
-    #         if match:
-    #             concat.append(b)
-
-
-    primes = list(primes_sieve(10000))
-    concat_dict = {}
-
-    for a in primes_sieve(50):
+    for a in primes_sieve(10000):
         print a
-        concat_list = concat_dict.get(a)
-        if concat_list == None:
-            concat_list = []
-        for b in primes:
-            if concat_prime(a, b):
-                concat_list.append(b)
-        concat_dict[a] = concat_list
+        concat = [a]
+        for b in primes_sieve(10000):
+            if len(concat) == 5:
+                print concat
+                return sum(concat)
+            match = True
+            for c in concat:
+                if not concat_prime(b, c):
+                    match = False
+                    break
+            if match:
+                concat.append(b)
 
 
-    for a in concat_dict:
-        concat_list = 
+    # primes = list(primes_sieve(10000))
+    # concat_dict = {}
+
+    # for a in primes_sieve(50):
+    #     print a
+    #     concat_list = concat_dict.get(a)
+    #     if concat_list == None:
+    #         concat_list = []
+    #     for b in primes:
+    #         if concat_prime(a, b):
+    #             concat_list.append(b)
+    #     concat_dict[a] = concat_list
+
+
+    # for a in concat_dict:
+    #     concat_list = 
 
 
 
